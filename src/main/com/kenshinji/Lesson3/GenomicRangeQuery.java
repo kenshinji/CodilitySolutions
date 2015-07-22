55d306d81314bf9943b5ec24ffc905edf7769338
kenshinji <gundam0083ster@gmail.com>
2015-07-22 17:38:26 +0800

    added new code

---
+++ src/main/com/kenshinji/Lesson3/GenomicRangeQuery.java
@@ -0,0 +1,20 @@
+/*
+ * @(#)GenomicRangeQuery.java 1.0 2015年7月22日
+ *
+ * Copyright 2015 Hewlett-Packard, Inc. All rights reserved.
+ * HP PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.kenshinji.Lesson3;
+
+/**
+ * 
+ * 
+ * @author Fred Wang 上午9:22:21
+ * @since HPDM 4.7
+ */
+public class GenomicRangeQuery {
+    public static int[] solution(String S, int[] P, int[] Q) {
+        // write your code in Java SE 8
+        return new int[] {};
+    }
+}
---
+++ src/test/com/kenshinji/Lesson3/GenomicRangeQueryTest.java
@@ -0,0 +1,26 @@
+/*
+ * @(#)GenomicRangeQueryTest.java 1.0 2015年7月22日
+ *
+ * Copyright 2015 Hewlett-Packard, Inc. All rights reserved.
+ * HP PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.kenshinji.Lesson3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * 
+ * 
+ * @author Fred Wang 上午9:22:36
+ * @since HPDM 4.7
+ */
+public class GenomicRangeQueryTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
