13d41b97e0fdf24e6e38e83fa20bd5b736cefa81
Fred Wang <gundam0083ster@gmail.com>
2015-07-26 10:13:14 +0800

    added solution for MinAvgTwoSlice

--- src/main/com/kenshinji/Lesson3/MinAvgTwoSlice.java
+++ src/main/com/kenshinji/Lesson3/MinAvgTwoSlice.java
@@ -1,18 +1,33 @@
-/*
- * @(#)MinAvgTwoSlice.java 1.0 2015Äê7ÔÂ21ÈÕ
- *
- */
+
 package com.kenshinji.Lesson3;
 
-/**
- * 
- * 
- * @author Fred Wang ÉÏÎç11:14:10
- * @since HPDM 4.7
- */
+
 public class MinAvgTwoSlice {
-    public static int solution(int[] A) {
-        // write your code in Java SE 8
-        return 1;
-    }
+	// this solution is based on the fact that there must be minimal average slice
+	// of length 2 or 3 in all the minimal average slices.
+	// we can use the slice of length 4 and 5 to prove it.
+	// if there is a slice of length 4 is the minimal average slice, then
+	// (a1+a2+a3+a4)/4 <= (a1+a2)/2 -> a3+a4 <= a1+a2
+	// (a1+a2+a3+a4)/4 <= (a3+a4)/2 -> a1+a2 <= a3+a4
+	// so a1+a2 = a3+a4 -> (a1+a2+a3+a4)/4 = (a1+a2)/2
+	// if there is a slice of length 5 is the minimal average slice, then
+	// we can use the same method to prove it.
+	// it's easy to conclude that (a1+a2+a3+a4+a5)/5 = (a1+a2)/2 = (a3+a4+a5)/3.
+	
+	// the trick of this solution is convert comparing devision into comparing multiplication， in order to avoiding 
+	// the lost of precision.
+	public static int solution(int[] A) {
+		long min = Long.MAX_VALUE;
+		int index = -1;
+		for(int i = 0;i<A.length-2;i++){
+			if(min > Math.min((A[i]+A[i+1])*3, (A[i]+A[i+1]+A[i+2])*2)){
+				min = Math.min((A[i]+A[i+1])*3, (A[i]+A[i+1]+A[i+2])*2);
+				index = i;
+			}
+		}
+		if((A[A.length-2]+A[A.length-1])*3<min){
+			index = A.length-2;
+		}
+		return index;
+	}
 }
