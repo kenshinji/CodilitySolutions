310e8026ba0732d04e739fe35792dc879fa27514
Fred Wang <gundam0083ster@gmail.com>
2015-07-27 23:10:22 +0800

    added solution for GenomicRangeQuery

--- src/main/com/kenshinji/Lesson3/GenomicRangeQuery.java
+++ src/main/com/kenshinji/Lesson3/GenomicRangeQuery.java
@@ -1,20 +1,51 @@
-/*
- * @(#)GenomicRangeQuery.java 1.0 2015年7月22日
- *
- * Copyright 2015 Hewlett-Packard, Inc. All rights reserved.
- * HP PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 package com.kenshinji.Lesson3;
 
-/**
- * 
- * 
- * @author Fred Wang 上午9:22:21
- * @since HPDM 4.7
- */
+import java.util.HashMap;
+
 public class GenomicRangeQuery {
-    public static int[] solution(String S, int[] P, int[] Q) {
-        // write your code in Java SE 8
-        return new int[] {};
-    }
+
+	public static int[] solution(String S, int[] P, int[] Q) {
+		// write your code in Java SE 8
+		int N = S.length();
+		int M = P.length;
+		int[] result = new int[M];
+		// store the position of A in the String S
+		int[] posOfA = new int[N + 1];
+		int[] posOfC = new int[N + 1];
+		int[] posOfG = new int[N + 1];
+		int[] posOfT = new int[N + 1];
+		for (int i = 0; i < N; i++) {
+			// when A appears, assign 1 to the value of posOfA with the same
+			// index.
+			if (S.charAt(i) == 'A')
+				posOfA[i + 1] = 1;
+			if (S.charAt(i) == 'C')
+				posOfC[i + 1] = 1;
+			if (S.charAt(i) == 'G')
+				posOfG[i + 1] = 1;
+			if (S.charAt(i) == 'T')
+				posOfT[i + 1] = 1;
+		}
+		for (int i = 1; i <= N; i++) {
+			// prefix sum of the array
+			posOfA[i] += posOfA[i - 1];
+			posOfC[i] += posOfC[i - 1];
+			posOfG[i] += posOfG[i - 1];
+			posOfT[i] += posOfT[i - 1];
+		}
+		for (int i = 0; i < M; i++) {
+			// calculate the sum between P[i] and Q[i] in O(1) time complexity
+			// if the value is not zero, then A has appeared in the slice.
+			if ((posOfA[Q[i] + 1] - posOfA[P[i]]) != 0)
+				result[i] = 1;
+			else if ((posOfC[Q[i] + 1] - posOfC[P[i]]) != 0)
+				result[i] = 2;
+			else if ((posOfG[Q[i] + 1] - posOfG[P[i]]) != 0)
+				result[i] = 3;
+			else if ((posOfT[Q[i] + 1] - posOfT[P[i]]) != 0)
+				result[i] = 4;
+		}
+		return result;
+	}
+
 }
--- src/test/com/kenshinji/Lesson3/GenomicRangeQueryTest.java
+++ src/test/com/kenshinji/Lesson3/GenomicRangeQueryTest.java
@@ -1,26 +1,16 @@
-/*
- * @(#)GenomicRangeQueryTest.java 1.0 2015年7月22日
- *
- * Copyright 2015 Hewlett-Packard, Inc. All rights reserved.
- * HP PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
- */
 package com.kenshinji.Lesson3;
 
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-/**
- * 
- * 
- * @author Fred Wang 上午9:22:36
- * @since HPDM 4.7
- */
 public class GenomicRangeQueryTest {
+	private String DNSSequence = "CAGCCTA";
+	int[] P = { 2, 5, 0 };
+	int[] Q = { 4, 5, 6 };
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
-    }
-
+	@Test
+	public void test() {
+		assertArrayEquals(new int[]{2,4,1}, GenomicRangeQuery.solution(DNSSequence, P, Q));
+	}
 }
