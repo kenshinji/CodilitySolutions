30c0d42a6928e44272b9e9ea18b83bef4933c96f
Fred Wang <gundam0083ster@gmail.com>
2015-08-20 21:27:06 +0800

    added solution for MaxDoubleSliceSum

---
+++ src/test/com/kenshinji/Lesson7/MaxDoubleSliceSumTest.java
@@ -0,0 +1,14 @@
+package com.kenshinji.Lesson7;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MaxDoubleSliceSumTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
