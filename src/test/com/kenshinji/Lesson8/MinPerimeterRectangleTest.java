75e3af41b81a9fa14363bbd7a27f52801d7ad89c
kenshinji <gundam0083ster@gmail.com>
2015-09-07 11:55:20 +0800

    added solution for MinPerimeterRectangle

---
+++ src/test/com/kenshinji/Lesson8/MinPerimeterRectangleTest.java
@@ -0,0 +1,16 @@
+
+package com.kenshinji.Lesson8;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class MinPerimeterRectangleTest {
+
+    @Test
+    public void test() {
+        assertEquals(22, MinPerimeterRectangle.solution(30));
+    }
+
+}
