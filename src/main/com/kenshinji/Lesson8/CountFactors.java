e44ea0adac8f28017b37417c965540c98e3ba8f7
kenshinji <gundam0083ster@gmail.com>
2015-09-08 12:20:49 +0800

    added solution for CountFactors

---
+++ src/main/com/kenshinji/Lesson8/CountFactors.java
@@ -0,0 +1,34 @@
+package com.kenshinji.Lesson8;
+
+/*
+ * A positive integer D is a factor of a positive integer N if there exists an integer M such that N = D * M.
+ * For example, 6 is a factor of 24, because M = 4 satisfies the above condition (24 = 6 * 4).
+ * Write a function:
+ * class Solution { public int solution(int N); }
+ * that, given a positive integer N, returns the number of its factors.
+ * For example, given N = 24, the function should return 8, because 24 has 8 factors, namely 1, 2, 3, 4, 6, 8, 12, 24. There are no other factors of 24.
+ * Assume that:
+ * N is an integer within the range [1..2,147,483,647].
+ * Complexity:
+ * expected worst-case time complexity is O(sqrt(N));
+ * expected worst-case space complexity is O(1).
+ */
+public class CountFactors {
+    public static int solution(int N) {
+        // write your code in Java SE 8
+        int Nsqrt = (int) Math.sqrt(N) + 1;
+        int count = 0;
+        for (int i = 1; i < Nsqrt; i++) {
+            if (N % i == 0) {
+                if (N / i == i) {
+                    count++;
+                }
+                else {
+                    count += 2;
+                }
+            }
+
+        }
+        return count;
+    }
+}
---
+++ src/test/com/kenshinji/Lesson8/CountFactorsTest.java
@@ -0,0 +1,15 @@
+package com.kenshinji.Lesson8;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+
+public class CountFactorsTest {
+
+    @Test
+    public void test() {
+        assertEquals(8, CountFactors.solution(24));
+    }
+
+}
