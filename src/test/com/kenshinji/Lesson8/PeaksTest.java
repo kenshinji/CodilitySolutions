5b1847647a5552dc909d168715c962a6295c05e4
Fred Wang <gundam0083ster@gmail.com>
2015-09-13 11:23:43 +0800

    comments for GenomicRangeQuery added

---
+++ src/test/com/kenshinji/Lesson8/PeaksTest.java
@@ -0,0 +1,14 @@
+package com.kenshinji.Lesson8;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PeaksTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
