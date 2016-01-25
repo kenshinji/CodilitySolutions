d4c43cf288cd0b0b4342efd240c21cccc967d8b7
kenshinji <gundam0083ster@gmail.com>
2016-01-25 21:42:58 +0800

    finished BinaryGap in Java

--- src/test/com/kenshinji/Lesson1/BinaryGapTest.java
+++ src/test/com/kenshinji/Lesson1/BinaryGapTest.java
@@ -2,6 +2,8 @@ package com.kenshinji.Lesson1;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by kenshinji on 1/25/16.
  */
@@ -9,7 +11,7 @@ public class BinaryGapTest {
 
     @Test
     public void test(){
-
+        assertEquals(2,BinaryGap.solution(9));
     }
 
 }
