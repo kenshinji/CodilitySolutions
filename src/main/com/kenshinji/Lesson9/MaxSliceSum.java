8078ecda5e98dfccae09713c3f09574fe1b6e9e1
kenshinji <gundam0083ster@gmail.com>
2016-04-16 20:35:11 +0800

    finished MaxSliceSum

--- src/main/com/kenshinji/Lesson9/MaxSliceSum.java
+++ src/main/com/kenshinji/Lesson9/MaxSliceSum.java
@@ -4,11 +4,12 @@ package com.kenshinji.Lesson9;
 public class MaxSliceSum {
     public static int solution(int[] A) {
         // write your code in Java SE 8
-    		int maxsum = 0;
-    		int cursum = 0;
-    		for(int i=0;i<A.length;i++){
+		int maxsum = A[0];
+		int cursum = A[0];
+
+		for(int i=1;i<A.length;i++){
     			cursum += A[i];
-    			cursum = Math.max(0, cursum);
+    			cursum = Math.max(A[i], cursum);
     			maxsum = Math.max(cursum,maxsum);
     		}
     		return maxsum;
